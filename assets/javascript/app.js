6faa16a81326431075caccb5ace9ab9caad7ec6e
Kevin Patel <kpatel1209@gmail.com>
2019-01-04 09:40:25 -0500

    overdue push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
-// 1) When the page loads, the list of comedians below will appear inside buttons.
-// 2) 
-
-
+/*
+GIPHY API KEY: YfTTp6H2HDR02OTPxGMDAvq0j4waEWYa
+HOST: api.giphy.com
+PATH: /v1/gifs/search
+*/ 
+ 
 let comedians = ["Jerry Seinfeld",
                 "Kevin Hart",
                 "Dave Chappelle",
@@ -12,27 +14,53 @@ let comedians = ["Jerry Seinfeld",
                 "Wanda Sykes",
                 "Seth Rogen",
                 "Amy Schumer",
-                "Melissa McCarthy"];
+                "Melissa McCarthy",
+                "Eddie Murphy"];
 let gifCount = 10;
 let limitRating = "PG";
 
-function createButtons() {
+
+function arrayButtons() {
     for(let i = 0; i < comedians.length; i++) {
-		let comedianButton = $("<button>");
+        let comedianButton = $("<button>");
+        comedians.sort();
         comedianButton.text(comedians[i]);
         comedianButton.addClass("comedianButton");
-		$("#comedian-buttons").append(comedianButton);
+        $("#comedian-buttons").append(comedianButton);  
     }
     
     $(".comedianButton").on("click", function(){
-        
-    })
-	
+    });
+}
+
+function createButton(people){
 
 }
 
+function searchGiphy(people){
+    const endpoint = "http://api.giphy.com/v1/gifs/search?q=" + people;
+    const params = + "&api_key=YfTTp6H2HDR02OTPxGMDAvq0j4waEWYa&rating=" + limitRating + "&limit=" + gifCount;
+    const url = endpoint + params;
+    console.log(url);
+        
+    $.ajax({
+        url: url,
+        method: "GET",
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
-    createButtons();
+    arrayButtons();
 
 });
 
