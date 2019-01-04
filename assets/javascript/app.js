01bf745c4da7cca3abdc6bb0407051c28308a732
Kevin Patel <kpatel1209@gmail.com>
2019-01-04 14:17:24 -0500

    added .empty() to both button functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
 /*
-GIPHY API KEY: YfTTp6H2HDR02OTPxGMDAvq0j4waEWYa
 HOST: api.giphy.com
+GIPHY API KEY: YfTTp6H2HDR02OTPxGMDAvq0j4waEWYa
 PATH: /v1/gifs/search
 */ 
- 
+
+// GLOBAL VARIABLES
 let comedians = ["Jerry Seinfeld",
                 "Kevin Hart",
                 "Dave Chappelle",
-                "Garbiel Iglesias",
+                "Robin Williams",
                 "Russell Peters",
                 "Chris Rock",
                 "Ellen Degeneres",
@@ -18,50 +19,73 @@ let comedians = ["Jerry Seinfeld",
                 "Eddie Murphy"];
 let gifCount = 10;
 let limitRating = "PG";
+let person = $(".user-input").val()
 
+$(document).ready(function(){
 
+// Takes the array of comedians and creates buttons for each person when the page loads.
 function arrayButtons() {
     for(let i = 0; i < comedians.length; i++) {
         let comedianButton = $("<button>");
+        // The comedian names will be sorted in alphabetical order.
         comedians.sort();
         comedianButton.text(comedians[i]);
         comedianButton.addClass("comedianButton");
-        $("#comedian-buttons").append(comedianButton);  
-    }
-    
-    $(".comedianButton").on("click", function(){
+        $("#comedian-buttons").append(comedianButton);      
+    };
+    // This function will 
+    $(".comedianButton").on("click", function() {
+        $(".gifs").empty();
+        searchGiphy($(this).text());
     });
 }
 
-function createButton(people){
-
+function buttonAddComedian(person) {
+    if(comedians.indexOf(person) === -1) {
+        comedians.push(person);
+        $("#comedian-buttons").empty();
+        arrayButtons();
+    }
 }
 
-function searchGiphy(people){
-    const endpoint = "http://api.giphy.com/v1/gifs/search?q=" + people;
-    const params = + "&api_key=YfTTp6H2HDR02OTPxGMDAvq0j4waEWYa&rating=" + limitRating + "&limit=" + gifCount;
-    const url = endpoint + params;
-    console.log(url);
-        
+function searchGiphy(person) {    
     $.ajax({
-        url: url,
+        url: "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=YfTTp6H2HDR02OTPxGMDAvq0j4waEWYa&rating=" + 
+        limitRating + "&limit=" + gifCount,
         method: "GET",
-    })
+    }).then(function(response) {
+        response.data.forEach(function(element) {
+            let addDiv = $("<div>");
+            addDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+            addDiv.addClass("gif-box");
+            let newGiphy = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+            newGiphy.attr("state", "still");
+            newGiphy.attr("still-data", element.images.fixed_height_still.url);
+            newGiphy.attr("animated-data", element.images.fixed_height.url);
+            newGiphy.addClass("giphy");
+            addDiv.append(newGiphy);
+            $(".gifs").append(addDiv);
+        })
+        
+        $(".giphy").unbind("click");
+        $(".giphy").on("click", function() {
+            if ($(this).attr("state") === "still") {
+                $(this).attr("state", "animated");
+                $(this).attr("src", $(this).attr("animated-data"));
+            } else {
+                $(this).attr("state", "still");
+                $(this).attr("src", $(this).attr("still-data"));
+            }
+        });
+    });
 }
 
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
     arrayButtons();
-
+    $("#add-comedian").on("click", function() {
+        event.preventDefault();
+        buttonAddComedian($("#user-input").val().trim());
+        $("#user-input").val("");
+    });
 });
 
 
